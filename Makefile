1d7678c1c4da7f3b9f9505bf8304b6d8a0a62357
MkfsSion <myychina28759@gmail.com>
2018-02-12 09:58:56 +0800

    Add cpu check when buiding.

--- Makefile
+++ Makefile
@@ -3,11 +3,15 @@ SRCS+=main.c
 SRCS+=audioplayer.c
 SRCS+=audiodecoder.c
 SRCS+=uriplayer.c
-SRCS+=urimain.c
 
+ifeq ($(findstring arm,$(shell uname -m)), arm)
+	LIBPATH:=/system/lib
+else ifeq ($(findstring aarch64,$(shell uname -m)), aarch64)
+	LIBPATH:=/system/lib64
+endif
 OBJS=$(SRCS:.c=.o)
 CFLAGS+=-I. -std=c99
-LDFLAGS+= -lOpenSLES -lavformat -lavcodec -lswresample -lavutil -llog -lm -lz -Wl,-rpath=/system/lib64
+LDFLAGS+= -lOpenSLES -lavformat -lavcodec -lswresample -lavutil -llog -lm -lz -Wl,-rpath=$(LIBPATH)
 
 all: player
 player: $(OBJS)
--- main.c
+++ main.c
@@ -4,12 +4,18 @@
 #include <pthread.h>
 #include "audioplayer.h"
 extern void *timer(void *);
-int main(void){
-  int result = CreatePlayerInstance("/sdcard/netease/cloudmusic/Music/水城新人 - 花葬.mp3");
+int main(int argc,char **argv){
+  if (argc!=2)
+  {
+    printf("Usage: openslplay <filepath>\n");
+    return -1;
+  }
+  char *uri=argv[1];
+  int result = CreatePlayerInstance(uri);
   if (result != 0) {
     printf("Failed to create player!\n");
+    return -1;
   }
-  printf("Created\n");
   StartPlay();
   pthread_t timerid;
   int r=pthread_create(&timerid, NULL, timer, NULL);
@@ -17,24 +23,16 @@ int main(void){
     return -1;
   }
   pthread_join(timerid, NULL);
-  result = CreatePlayerInstance("/sdcard/test.mp3");
-  if (result != 0) {
-    return -1;
-  }
-  StartPlay();
-  pthread_t timer2;
-  pthread_create(&timer2, NULL, timer, NULL);
-  pthread_join(timer2,NULL);
   return 0;
 }
 
 void* timer(void *context){
   while (getPlayState() == SL_PLAYSTATE_PLAYING) {
     sleep(1);
-   // if (getPlayState() != SL_PLAYSTATE_PLAYING) {
-   //   pthread_exit(NULL);
-   // }
-    printf("Position:%d\n", getPlayPosition());
+    if (getPlayState() != SL_PLAYSTATE_PLAYING) {
+      pthread_exit(NULL);
+    }
+    printf("Position:%d\n", getPlayPosition()/1000);
   }
   return NULL;
 }
